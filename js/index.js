d696152c0bbb5db5bd165172290d3c79071068f3
HK\brmak <makpt430@hotmail.com>
2017-01-14 16:59:13 +0800

    add temp login logic

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function check_form() {
+  if ($('input[name="user"]') == 'test' && $('input[name="pass"]') == '123456') {
+    location.href = 'redemption.htm';
+  } else {
+    location.href = 'index.htm?login_fail=1';
+  }
+}
+
+$(document).ready(function(){
+  url = location.href;
+  if (url.indexOf('login_fail=1') > 0) {
+    $('.msg_login_fail').show();
+  }
+});
